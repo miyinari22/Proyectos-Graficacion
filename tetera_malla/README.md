a56024ada65037c202c1e5e1fa74651b2b2f7ffd
Octavio Martínez <77460357+miyinari22@users.noreply.github.com>
2023-12-07 13:29:06 -0600

    Add files via upload

---
+++ tetera_malla/README.md
@@ -0,0 +1,9 @@
+# Programa de Visualización de Tetera en OpenGL
+
+Este programa en C utiliza la biblioteca GLUT para crear una ventana que muestra una tetera en 3D.
+
+## Funcionamiento del Código
+
+- El programa crea una ventana que muestra una tetera en 3D.
+- La función `inicializa` configura los parámetros de la ventana, estableciendo el color de fondo, la visualización y la proyección.
+- La función `pinta` dibuja una tetera alámbrica en la ventana.
---
+++ tetera_malla/tetera_malla.c
@@ -0,0 +1,29 @@
+/*Alumno: Octavio Augusto Martinez Reyes     	Matricula:201904948 */
+#include <GL/glut.h>
+#include <stdio.h>
+
+void inicializa(void){ /*Inicializa la ventana */
+	glClearColor(1.0,1.0,1.0,0.0); /*Cambia el color de la ventana a color negro*/
+	glMatrixMode(GL_MODELVIEW);
+	gluLookAt(0,0,5,0,0,0,0,1,5);
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(45,1,1,12);
+}
+	
+void pinta(void){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0,1,0); /*Pinta las lineas de color blanco*/
+	glutWireTeapot(1.0);
+	glFlush();
+}
+
+
+int main(int argc, char** argv){
+	glutInitWindowSize(400,400);
+	glutInit(&argc, argv);
+	glutCreateWindow("Martinez Reyes Octavio Augusto");
+	inicializa();
+	glutDisplayFunc(pinta);
+	glutMainLoop();
+	return 0;
+}
